326775737342d36f47d1243f9f7fcfcc62028f49
bifshteksex <roman_shangin@vk.com>
2024-03-12 03:11:17 +0800

    feat: 3:04 AM

--- libcom/main/static/css/script.js
+++ libcom/main/static/css/script.js
@@ -56,4 +56,124 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         })   
     }
+
+
+    // Intercept form submission
+    $('.save-rating').on('click', function(event) {
+        const modal = $(this).parent().parent()
+
+        const general = modal.find('.general-rating').find('input[type="radio"]:checked').val()
+        const plot = modal.find('.plot-rating').find('input[type="radio"]:checked').val()
+        const characters = modal.find('.characters-rating').find('input[type="radio"]:checked').val()
+        const style = modal.find('.style-rating').find('input[type="radio"]:checked').val()
+        const review = modal.find('[name="review"]').val()
+
+        const formData = new FormData()
+        formData.append('general', general)
+        formData.append('plot', plot)
+        formData.append('characters', characters)
+        formData.append('style', style)
+        formData.append('review', review)
+
+        $(modal).find('.save-rating').hide()
+        $(modal).find('.modal-footer').append('<div class="spinner-border" role="status"></div>')
+
+        $.ajax({
+            type: 'POST',
+            url: $(this).data('action'), // Use the form's action attribute as URL
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function(xhr, settings) {
+                // Get CSRF token from cookies
+                var csrftoken = getCookie('csrftoken');
+                
+                // Set CSRF token in request headers
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(response) {
+                $(modal).find('.save-rating').show()
+                $(modal).find('.spinner-border').remove()
+                $(modal).find('.btn-close').click()
+
+                $('.alert-complete').find('.text-alert-input').text('Оценка успешно добавлена')
+                $('.alert-complete').css('display', 'flex')
+                $('.alert-complete').show('slow')
+                setTimeout(() => {  $('.alert-complete').hide('slow') }, 2000);
+            },
+            error: function(xhr, errmsg, err) {
+                // Handle error if needed
+                console.log(xhr.status + ": " + xhr.responseText); // Log error message
+            }
+        })
+    })
+
+        // Intercept form submission
+    $('.want-read-btn').on('click', function(event) {
+        $.ajax({
+            type: 'POST',
+            url: $(this).data('action'),
+            processData: false,
+            contentType: false,
+            beforeSend: function(xhr, settings) {
+                // Get CSRF token from cookies
+                var csrftoken = getCookie('csrftoken');
+                
+                // Set CSRF token in request headers
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(response) {
+                $('.alert-complete').find('.text-alert-input').text('Вы успешно добавили книгу в Хочу прочитать')
+                $('.alert-complete').css('display', 'flex')
+                $('.alert-complete').show('slow')
+                setTimeout(() => {  $('.alert-complete').hide('slow') }, 2000);
+            },
+            error: function(xhr, errmsg, err) {
+                // Handle error if needed
+                console.log(xhr.status + ": " + xhr.responseText); // Log error message
+            }
+        })
+    })
+
+    $('.read-now-btn').on('click', function(event) {
+        $.ajax({
+            type: 'POST',
+            url: $(this).data('action'),
+            processData: false,
+            contentType: false,
+            beforeSend: function(xhr, settings) {
+                // Get CSRF token from cookies
+                var csrftoken = getCookie('csrftoken');
+                
+                // Set CSRF token in request headers
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            success: function(response) {
+                $('.alert-complete').find('.text-alert-input').text('Вы успешно добавили книгу в Читаю сейчас')
+                $('.alert-complete').css('display', 'flex')
+                $('.alert-complete').show('slow')
+                setTimeout(() => {  $('.alert-complete').hide('slow') }, 2000);
+            },
+            error: function(xhr, errmsg, err) {
+                // Handle error if needed
+                console.log(xhr.status + ": " + xhr.responseText); // Log error message
+            }
+        })
+    })
+
+    // Function to get CSRF token from cookies
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
 })
--- libcom/mybooks/urls.py
+++ libcom/mybooks/urls.py
@@ -5,5 +5,8 @@ app_name = "mybooks"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('statistic.html', views.statistic, name='statistic'),
     path('save_rating/<str:isbn>/', views.save_rating, name='save_rating'),
+    path('save_read/<str:isbn>/', views.save_read, name='save_read'),
+    path('save_want/<str:isbn>/', views.save_want, name='save_want'),
     ]
